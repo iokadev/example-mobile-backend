139227b974a43ae270124c92e6a518c1c38286d8
Aidar <a.nugmanoff@gmail.com>
2022-02-18 23:06:57 +0600

    remove redundant code

--- server.js
+++ server.js
@@ -11,50 +11,42 @@ app.use(express.json());
 
 const user = {
   customer_id: "9PzJbto7sc",
-  external_id: "3",
-  customer_access_token:
-    "9PzJbto7sc_secret_4224cc6a311fb77bcf3c8888a1444b364c1ccfc15d69a8116f114197d889456b",
+  external_id: "3"
 };
 
 app.post("/checkout", async (req, res) => {
-  const price = req.body["price"];
+  const price = req.body.price;
 
   if (!price) {
     res.status(400);
-    res.json({ message: "error: price must be present" });
+    res.json({ error: "price must be present!" });
     return;
   }
 
   try {
     const order_access_token = await createOrder(price);
-    const customer_access_token = await getCustomerById(
-      "9PzJbto7sc",
-      "HkSJ8mXwu0_secret_a8783e862e89011aaef1b0bb05e1e0815c2dde63125d8403b871732e554db2ca"
-    );
+    const customer_access_token = await getCustomerById(user.customer_id);
     res.status(200);
     res.json({ order_access_token, customer_access_token });
   } catch (error) {
-    res.json({ message: error.message });
+    res.json({ error: error.message });
   }
 });
 
 app.get("/profile", async (req, res) => {
-  const token = await getCustomerById(
-    "9PzJbto7sc",
-    "HkSJ8mXwu0_secret_a8783e862e89011aaef1b0bb05e1e0815c2dde63125d8403b871732e554db2ca"
-  );
+  const token = await getCustomerById(user.customer_id);
   res.json({ token: token });
 });
 
 const createOrder = async (price) => {
   const result = await fetch(`${BASE_URL}/orders`, {
-    method: "post",
+    method: "POST",
     body: JSON.stringify({ amount: price }),
     headers: { "Content-Type": "application/json", "API-KEY": API_KEY },
   });
   const response = await result.json();
   return response.order.access_token;
-}
+};
 
 const createCustomer = async (external_id) => {
   const result = await fetch(`${BASE_URL}/customers`, {
@@ -64,15 +56,15 @@ const createCustomer = async (external_id) => {
   });
   const response = await result.json();
   return response.customer.access_token;
-}
+};
 
-const getCustomerById = async (customer_id, customer_access_token) => {
+const getCustomerById = async (customer_id) => {
   const result = await fetch(`${BASE_URL}/customers/${customer_id}`, {
-    method: "GET"
+    method: "GET",
   });
   const response = await result.json();
   return response.access_token;
-}
+};
 
 app.listen(PORT, function (err) {
   if (err) console.log(err);
